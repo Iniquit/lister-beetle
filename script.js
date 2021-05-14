3edae18856493b2cdf5a4a5d745d3e8399cc0646
Iniquit <iniquitwrites@gmail.com>
2021-05-14 16:39:31 -0400

    5-14-21

--- script.js
+++ script.js
@@ -1,41 +1,81 @@
-const array = [];
-
-function Run() {
-  let url =
-    'https://api.scryfall.com/cards/random?q=legal%3Acommander+-is%3Afunny+-t%3Aland+-identity%3Dc+-is%3Avanilla';
-    //legal in Commander and the cards aren’t funny and the card types exclude “land” and the cards don’t have colorless identity and aren't vanilla
-  url += '+identity%3A' + getCurrentColors();
-  fetch(url)
-    .then((data) => {
-      return data.json();
-    })
-    .then((res) => {
-      if (array.includes(res.name) === false) {
-        array.push(res.name);
-
-        console.log(res.name);
-        document.getElementsByClassName('choose-number')[0].innerHTML = '';
-        document.getElementsByClassName('button')[0].innerHTML = '';
-        document.getElementsByClassName('count')[0].innerHTML =
-          array.length + ' cards';
-        document.getElementsByClassName(
-          'target',
-        )[0].innerHTML = array.sort().join('\n');
-      } else Run();
-    });
+async function getScryfallFull() {
+  return (await fetch('https://c2.scryfall.com/file/scryfall-bulk/oracle-cards/oracle-cards-20210411210506.json')).json();
 }
 
-function getCurrentColors() {
-  let identity = '';
+function fitsColorIdentiy(arr){
+  return (arr, target) => target.every(v => arr.includes(v));
+}
 
-  if (document.getElementById('white').checked) identity += 'w';
-  if (document.getElementById('blue').checked) identity += 'u';
-  if (document.getElementById('black').checked) identity += 'b';
-  if (document.getElementById('red').checked) identity += 'r';
-  if (document.getElementById('green').checked) identity += 'g';
-  if (identity == '') identity = 'c';
+const deck = [];
+let deckSize = document.getElementById('generateNumber').value
+let landCount = Math.floor(deckSize * 0.4)
+let spellCount = Math.floor(deckSize * 0.6)
+
+const Run = ()=> {
+
+  let spellList = []
+
+  getScryfallFull()
+  .then(res => res.filter(card => card.legalities.commander == 'legal'))
+  .then(res => res.filter(card => card.edhrec_rank <= document.getElementById('edhrec_limit').value))
+  .then(res => res.filter(card => !card.type_line.includes('Land')))
+  .then(res => res.filter(card => card.color_identity.every(color => getCurrentColors().includes(color))))
+
+  .then(allCards => {
+    
+    while (spellList.length < spellCount && spellList.length < allCards.length) {
+      let card = allCards[Math.floor(Math.random() * allCards.length)]
+      console.log(card)
+      if (spellList.includes(card) == false){
+        spellList.push(card)
+      }
+    }
+
+    deck.push(...spellList)
+
+    decklist = deck.map(obj => obj.name).sort()
+
+    
+
+    document.getElementsByClassName('choose-number')[0].innerHTML = '';
+    document.getElementsByClassName('button')[0].innerHTML = '';
+    document.getElementsByClassName('count')[0].innerHTML = decklist.length + ' spells';
+
+    addLands()
+    document.getElementsByClassName('target',)[0].innerHTML = decklist.join('\n');
+  })
+}
+
+
+
+
+function getCurrentColors() {
+  let identity = [];
+  if (document.getElementById('white').checked) identity.push('W');
+  if (document.getElementById('blue').checked) identity.push('U');
+  if (document.getElementById('black').checked) identity.push('B');
+  if (document.getElementById('red').checked) identity.push('R');
+  if (document.getElementById('green').checked) identity.push('G');
+  if (identity == '') identity.push('C');
 
   return identity;
+}
+
+function addLands() {
+  
+  let landsInEachColor = Math.floor(landCount / getCurrentColors().length)
+
+  getCurrentColors().forEach(res =>{
+    console.log(res)
+
+    if (res == 'W') decklist.push(`${landsInEachColor} Plains`)
+    if (res == 'U') decklist.push(`${landsInEachColor} Island`)
+    if (res == 'B') decklist.push(`${landsInEachColor} Swamp`)
+    if (res == 'R') decklist.push(`${landsInEachColor} Mountain`)
+    if (res == 'G') decklist.push(`${landsInEachColor} Forest`)
+    if (res == 'C') decklist.push(`${landsInEachColor} Wastes`)
+
+  })
 }
 
 function copyTextFromElement(elementID) {
